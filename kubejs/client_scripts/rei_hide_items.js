9d8c3f569be69dbcd83068d09ff6743c53e1d8cd
spoorn <michael.trinh1996@gmail.com>
2021-03-17 21:04:22 -0700

    Add foods to Nourish KubeJS data

--- kubejs/client_scripts/rei_hide_items.js
+++ kubejs/client_scripts/rei_hide_items.js
@@ -2,4 +2,6 @@ events.listen('rei.hide.items', event => {
     console.log("Hiding extra slime sling/boots");
     event.hide('kibe:slime_sling');
     event.hide('equip:slime_boots');
+
+    event.hide('techreborn:sapphire_sword');
 })
---
+++ kubejs/server_scripts/remove.js
@@ -0,0 +1,5 @@
+// remove items
+events.listen('recipes', function (event) {
+
+    event.remove({ output: "techreborn:sapphire_sword" });
+})
