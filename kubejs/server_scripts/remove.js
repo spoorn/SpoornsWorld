750fc5ea5b80430ff9ad5d96b8fa8d9f7ab02b3d
spoorn <michael.trinh1996@gmail.com>
2021-03-24 23:55:57 -0700

    Bump to 1.8
    - Removed air walking Gobber2 ring
    - Added Golemancy
    - Added Forge of Fiends
    - Updated various mods

--- kubejs/server_scripts/remove.js
+++ kubejs/server_scripts/remove.js
@@ -2,4 +2,7 @@
 events.listen('recipes', function (event) {
 
     event.remove({ output: "techreborn:sapphire_sword" });
+
+    // Causes server kick due to allow-flight=false in server.properties by default
+    event.remove({ output: 'gobber2:gobber2_ring_airwalking' });
 })
